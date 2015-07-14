981a2dbda320082b27cf206bf47ae7871c929b63
Michael <mr201190@gmail.com>
2015-07-14 10:00:38 +0200

    adds actions to user-controller and views

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,25 @@
 class UserController < ApplicationController
   def index
   end
+
+  def create
+  end
+
+  def new
+  end
+  
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def delete
+  end
+
+  def update
+  end
 end
