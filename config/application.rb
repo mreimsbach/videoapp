a3a8394df31d6fd4d906a779c5d74584cd3788bf
Sven Schmidt <sven@adherry.de>
2015-07-14 09:18:14 +0200

    f

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 #require "active_resource/railtie" # Comment this line for Rails 4.0+
 require "rails/test_unit/railtie"
+require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
