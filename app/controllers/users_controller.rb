f9a88cc651109fd6755ed81ada5b23aacfa3ad06
Michael <mr201190@gmail.com>
2015-07-15 09:47:49 +0200

    Adds basic function to user-controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    @user.save
   end
 
   def new
@@ -11,15 +13,24 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
   end
 
   def destroy
+    User.find(params[:id]).destroy
+  end
+
+  private
+  def user_params
+      params.require(:user).permit(:email, :first_name, :last_name)
   end
 
 
