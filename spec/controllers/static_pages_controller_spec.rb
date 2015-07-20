3a7231e740b559b314adcc2e46b5dce678607831
Sven Schmidt <sven@adherry.de>
2015-07-20 14:54:15 +0200

    adding test for static pages

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-
+  describe "get #home" do
+    subject { get :home }
+    render_views
+    it "should return the correct page" do
+      expect(subject).to render_template(:home)
+    end
+    
+  end
 end
