7a462985c8fc30d810d1a7146d8c14bacf2d1ccf
Sven Schmidt <sven@adherry.de>
2015-07-27 11:43:47 +0200

    more broken tests

--- spec/views/courses/index.html.erb_spec.rb
+++ spec/views/courses/index.html.erb_spec.rb
@@ -1,21 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "courses/index", type: :view do
-  before(:each) do
-    assign(:courses, [
-      Course.create!(
-        :name => "Name",
-        :description => "Description"
-      ),
-      Course.create!(
-        :name => "Name",
-        :description => "Description"
-      )
-    ])
-  end
+  # before(:each) do
+  #   assign(:courses, [
+  #     create(:course),
+  #     create(:course)
+  #   ])
+  # end
 
   it "renders a list of courses" do
-    render
+    @channel = create(:channel)
+    render(channel_id: @channel.id)
     assert_select "tr>td", :text => "Name".to_s, :count => 2
     assert_select "tr>td", :text => "Description".to_s, :count => 2
   end
