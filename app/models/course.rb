fef0d810abc25a45c78149dd8b896889f2feb4d7
Sven Schmidt <sven@adherry.de>
2015-08-04 08:12:48 +0200

    making relation between video and course

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,7 +4,7 @@ class Course
   validates :description, length: { maximum: 1000 }
 
   embedded_in :channel
-
+  has_many :videos
   field :name, type: String
   field :description, type: String, default: ""
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -2,7 +2,7 @@ class Video
   include Mongoid::Document
   mount_uploader :file, VideoUploader
 
-  belongs_to :channel
+  belongs_to :course
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 1000 }
 
