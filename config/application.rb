019f4139bd2c88c8a4da0c6ad9d5d4502b363d97
Sven Schmidt <sschmidt@avarteq.de>
2015-08-12 12:31:13 +0200

    never trust the internet

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ Bundler.require(*Rails.groups)
 
 module Videoapp
   class Application < Rails::Application
-    config.action_mailer.async = true
+    
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
