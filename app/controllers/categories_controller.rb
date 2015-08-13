0146706259425049276fdc57a6c73a03fe584feb
Sven Schmidt <sschmidt@avarteq.de>
2015-08-13 13:15:52 +0200

    only admin can now create categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :admin_user, only: [:new, :create]
 
   # GET /categories
   # GET /categories.json
@@ -67,6 +68,9 @@ class CategoriesController < ApplicationController
       @category = Category.find(params[:id])
     end
 
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
     # Never trust parameters from the scary internet, only allow the white list through.
     def category_params
       params.require(:category).permit(:tag)
