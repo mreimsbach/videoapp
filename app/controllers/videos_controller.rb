cdf31580d53e2fc0731ef92aa844e331e57cdfdb
Michael <mr201190@gmail.com>
2015-08-13 09:35:18 +0200

    display categories in video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -76,7 +76,7 @@ class VideosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def video_params
-      params.require(:video).permit(:name, :description, :file, :course_id, :channel_id)
+      params.require(:video).permit(:name, :description, :file, :course_id, :channel_id, :category_ids => [])
     end
 
     def new_filter
