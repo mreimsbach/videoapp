f32f586fd17cd45841d3247688b31b9346aebb61
Sven Schmidt <sschmidt@avarteq.de>
2015-08-13 08:26:59 +0200

    adding anynines support for carrierwave

--- Gemfile
+++ Gemfile
@@ -80,4 +80,6 @@ group :test do
 end
 group :production do
   gem 'rails_12factor'
+  gem 'fog'
+  gem 'a9s_swift'
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,3 +9,11 @@ if Rails.env.development?
     config.storage :file
   end
 end
+require 'carrierwave'
+require 'a9s_swift'
+
+BUCKET_NAME = "images"
+
+if Rails.env.production?
+  Anynines::Swift::Utility.configure_carrierwave(BUCKET_NAME,{fog_public: false}, "openstack")
+end
