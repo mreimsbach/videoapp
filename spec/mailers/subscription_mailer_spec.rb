00f351fa6a13cfc722631922b5d410a18b4b37dd
Michael <mr201190@gmail.com>
2015-08-18 13:29:25 +0200

    add tests for subscription mailer

--- spec/mailers/subscription_mailer_spec.rb
+++ spec/mailers/subscription_mailer_spec.rb
@@ -1,5 +1,41 @@
 require "rails_helper"
 
 RSpec.describe SubscriptionMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+    context "new_video" do
+      before (:example) do
+        @user = create(:user, channel: create(:channel_with_courses))
+        @video = create(:video)
+        @mail = SubscriptionMailer.new_video(@user, @user.channel, @video)
+      end
+      it 'renders the subject' do
+        expect(@mail.subject).to eql("One of your subscribed channels has been updated")
+      end
+
+      it 'renders the receiver email' do
+        expect(@mail.to).to eql([@user.email])
+      end
+
+      it 'renders the sender email' do
+        expect(@mail.from).to eql(['sschmidt@avarteq.de'])
+      end
+    end
+    context "new_course" do
+      before (:example) do
+        @user = create(:user, channel: create(:channel_with_courses))
+        @video = create(:video)
+        @mail = SubscriptionMailer.new_course(@user, @user.channel, @video)
+      end
+      it 'renders the subject' do
+        expect(@mail.subject).to eql("One of your subscribed channels has been updated")
+      end
+
+      it 'renders the receiver email' do
+        expect(@mail.to).to eql([@user.email])
+      end
+
+      it 'renders the sender email' do
+        expect(@mail.from).to eql(['sschmidt@avarteq.de'])
+      end
+    end
 end
