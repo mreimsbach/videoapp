455945d98a34be233959e6680c81d93640d4e8d8
Michael <mr201190@gmail.com>
2015-08-21 10:51:16 +0200

    add color for like / dislike button

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,6 +1,7 @@
 module VideosHelper
   def button_color(value)
     return "btn-default" unless @video.vote_value(current_user) == value
-    return "btn-success"
+    return "btn-success" if value == :up
+    return "btn-danger" if value == :down
   end
 end
